e3103128c82816a8eb48cb46177ea0de61d95af5
bala227 <115527760+bala227@users.noreply.github.com>
2025-03-23 18:12:44 +0530

    nlp

--- practive/src/App.js
+++ practive/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const handleTranslate = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/translate", {
+      const response = await fetch("https://luffyhelixul.pythonanywhere.com/translate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ sentence: inputText, lang_pair: targetLang }),
